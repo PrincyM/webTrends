f47cdb127b42be059d4f2bac5f8c12572d646f23
PrincyM <prinna.pm@gmail.com>
2018-05-28 19:13:07 -0400

    adding Lab 3 Part 2

--- Lab3/lab3.js
+++ Lab3/lab3.js
@@ -33,5 +33,51 @@ function initMap() {
         center: Toronto,
         zoom: 8
     });
-}
+    for (var _i = 0, addresses_2 = addresses; _i < addresses_2.length; _i++) {
+        var cl = addresses_2[_i];
+        var latlng = { lat: cl.lat, lng: cl.lon };
+        var newMapMarker = new MapMarker(cl.address);
+        mapMarkers.push(newMapMarker);
+    }
+    //let markersIndex: number = 0;
+    for (var i = 0; i < 10; i++) {
+        // setTimeout(() => {}, 1000);
+        setLatitudeLongitude(i);
+        // console.log(mapMarkers[i].Coordinates)
+        addMarker(mapMarkers[1].Coordinates);
+    }
+    function setLatitudeLongitude(markersIndex) {
+        //assigns lat and long for each map marker
+        mapMarkers[markersIndex].Coordinates = getLatLng(mapMarkers[markersIndex].Address);
+        //    setTimeout(() => {console.log(mapMarkers[markersIndex])}, 1000);
+        console.log(mapMarkers[markersIndex].Coordinates);
+    }
+    function getLatLng(address) {
+        geocoder.geocode({ address: address }, function (results, status) {
+            //console.log(status)
+            if (status === 'OK') {
+                console.log('Latitude : ' + results[0].geometry.location.lat());
+                console.log('Longitude : ' + results[0].geometry.location.lng());
+                var coord = {
+                    lat: results[0].geometry.location.lat(),
+                    lng: results[0].geometry.location.lng()
+                };
+                // console.log(coord);
+                return coord;
+            }
+            else {
+                console.log('In else');
+                setInterval(getLatLng(address), 2000);
+            }
+        });
+    }
+    function addMarker(coord) {
+        //will place mapmarker based on coordinates
+        var newMarker = new google.maps.Marker({
+            position: coord,
+            map: map,
+            title: "AC Locations to be at"
+        });
+    }
+} // end of initMap()
 initMap();
--- Lab3/lab3.ts
+++ Lab3/lab3.ts
@@ -1,7 +1,6 @@
 let map: any;
 let addresses : any[];
 let mapMarkers : MapMarker[] = []; // defining an array of MapMarker object & initializing it
-
 // interface
 interface LatLng {
     lat: number,
@@ -28,14 +27,12 @@ $.ajax({
         // for(let i of data){
         //     console.log(i.address);
         // }
-
         addresses =  data;
         for(let i of addresses){
             console.log(i.address);
 
             // add map marker to array of map markers
             let newMapMarker : MapMarker = new MapMarker(i.address); // using address constructor here
-
             mapMarkers.push(newMapMarker);
         }
 
@@ -52,7 +49,68 @@ function initMap(){
          zoom: 8
      }
    );
+
+   for(let cl of addresses) {
+    let latlng : LatLng = { lat: cl.lat, lng: cl.lon};
+    let newMapMarker : MapMarker = new MapMarker(cl.address);
+    mapMarkers.push(newMapMarker);
 }
 
-initMap();
+//let markersIndex: number = 0;
+    for(let i = 0; i < 10; i++) {
+        
+        // setTimeout(() => {}, 1000);
+        setLatitudeLongitude(i);
+        // console.log(mapMarkers[i].Coordinates)
+        addMarker(mapMarkers[1].Coordinates);
+    }
 
+        function setLatitudeLongitude(markersIndex : number) : void {
+            //assigns lat and long for each map marker
+            mapMarkers[markersIndex].Coordinates = getLatLng(mapMarkers[markersIndex].Address);  
+        //    setTimeout(() => {console.log(mapMarkers[markersIndex])}, 1000);
+        console.log(mapMarkers[markersIndex].Coordinates)
+        }
+
+
+
+        function getLatLng(address : string) : LatLng {
+        
+            geocoder.geocode(
+                {address: address},
+                function(results, status) {
+                    //console.log(status)
+                    if(status === 'OK') {
+                        console.log('Latitude : ' + results[0].geometry.location.lat());
+                        console.log('Longitude : ' + results[0].geometry.location.lng());
+                        var coord = {
+                            lat: results[0].geometry.location.lat(), 
+                            lng: results[0].geometry.location.lng() 
+                        };
+                        // console.log(coord);
+                        return coord;
+                        
+                    } else {
+                        console.log('In else');
+                        setInterval( getLatLng(address), 2000);
+                        
+                    }
+                }
+            );
+    
+       }
+
+
+       function addMarker(coord : LatLng) : void {
+        //will place mapmarker based on coordinates
+        
+        let newMarker = new google.maps.Marker({
+            position: coord,
+            map: map,
+            title: `AC Locations to be at`
+        });
+   }
+
+} // end of initMap()
+
+initMap();
